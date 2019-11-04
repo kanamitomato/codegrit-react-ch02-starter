fd5cc9edad771e48dde5141bc5ad440d6abde53d
Kanami <kanami.h217@gmail.com>
2019-11-04 20:41:23 +0900

    changed file name from CardBody.js to index.js

---
+++ src/InstaCard/body/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import CardImage from '../body/CardImage';
+import CardMain from '../body/CardMain';
+
+const CardBody = () => {
+    return (
+        <section className="card-body">
+            <CardImage />
+            <CardMain />
+        </section>
+    )
+}
+
+
+export default CardBody;
