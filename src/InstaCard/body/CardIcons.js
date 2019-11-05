6c764444c6e9da37ba8b24f68d32035d2a44d423
Kanami <kanami.h217@gmail.com>
2019-11-05 10:39:19 +0900

    changed file name

---
+++ src/InstaCard/body/CardIcons.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import BookmarkImg from '../../images/bookmark.png';
+import BubbleImg from '../../images/bubble.png';
+import HeartImg from '../../images/heart.png';
+
+const CardIcons = () => {
+    const imageStyle = {
+        width: '30px',
+        height: '30px'
+    }
+    return (
+        <div class="card-main-icons">
+            <ul className="flex-container card-main-icons-left">
+                <li className="icon-heart">
+                    <img src={HeartImg} style={imageStyle} />
+                </li>
+                <li className="icon-comment">
+                    <img src={BubbleImg} style={imageStyle} />
+                </li>
+            </ul>
+            <ul className="flex-container card-main-icons-right">
+                <li className="icon-bookmark icons-right">
+                    <img src={BookmarkImg} style={imageStyle} />
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default CardIcons;
